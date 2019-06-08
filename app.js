833d298c57ea40620e1007b1d7c7806e22e46cb0
Lei Wang <xwang@sandiego.edu>
2019-06-08 12:38:07 -0700

    implemented play store express server

--- app.js
+++ app.js
@@ -2,13 +2,54 @@ const express = require('express');
 const morgan = require('morgan');
 
 const app = express();
-
 app.use(morgan('common'));
 
-app.get('/apps', (req, res) => {
+const playstore = require('./playstore.js');
 
+app.get('/apps', (req, res) => {
+    const { sort, genre } = req.query;
+
+    if(!sort || !genre) {
+        return res
+            .status(200)
+            .json(playstore);
+    }
+
+    if(sort) {  // validation for sort
+        let lcSort = req.query.sort.toLowerCase();
+        if(!['rating', 'app'].includes(lcSort)) {
+            return res
+                .status(400)
+                .send('Sort must be one of Rating or App');
+        }
+    }
+
+    if(genre) {     // validation for genre filter
+        // console.log(lowerCaseGenre);
+        let lcGenre = req.query.genre.toLowerCase();
+        const allGenres = ['Action', 'Puzzle', 'Strategy', 'Casual', 'Arcade', 'Card'].map(     word  => word.toLowerCase());
+        if(!allGenres.includes(lcGenre)) {
+            return res 
+                .status(400)
+                .send('Genre must be one of Action, Puzzle, Strategy, Casual, Arcade, or Card');
+        }
+    }
+
+    let results = playstore.filter( playstoreApp =>
+        playstoreApp
+            .Genres.toLowerCase()
+            .includes(genre.toLowerCase()));
+
+    if(sort) {
+        results.sort((a, b) => {
+            return a[sort] > b[sort] ? 1 :
+            a[sort] < b[sort] ? -1 : 0;
+        });
+    }
+
+    res.json(results);
 });
 
 app.listen(8000, () => {
-    console.log('Server started at 8000');
+    console.log('Server started on PORT 8000');
 });
